276b3d9690b22cb7dc4a524e38ff3b0eefa2edda
isbrandon02 <brandon.chng1102@gmail.com>
2025-01-10 17:45:06 -0500

    Added home page

---
+++ client/vite-project/src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <>
+      <div className="home--container">
+        <div className="text--container">
+          <h1 className="home--title">ROAD</h1>
+          <h3 className="home--sub">Your Journey, Your Soundtrack</h3>
+        </div>
+        <button className="home--button">Get Started</button>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
